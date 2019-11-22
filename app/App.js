f95b2cf9831c8ff5b7c1523a232f375506c30d12
Joswells <wagnerjr300@gmail.com>
2019-11-22 15:34:02 -0300

    with '#' will be ignored, and an empty message aborts the commit.

---
+++ app/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import {View, Text} from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import Axios from 'axios';
+
+export default class App extends Component {
+  state = {
+    barcodes: [],
+  }
+
+  barcodeRecognized = ({ barcodes }) => {
+    Axios.get({''})
+    .then(Response => {
+      
+    })
+    this.setState({ barcodes })
+  };
+
+  renderBarcodes = () => (
+    <View>
+      {this.state.barcodes.map(this.renderBarcode)}
+    </View>
+  );
+
+  renderBarcode = ({ bounds, data }) => (
+    <React.Fragment key={data + bounds.origin.x}>
+      <View
+        style={{
+          borderWidth: 2,
+          borderRadius: 10,
+          position: 'absolute',
+          borderColor: '#F00',
+          justifyContent: 'center',
+          backgroundColor: 'rgba(255, 255, 255, 0.9)',
+          padding: 10,
+          ...bounds.size,
+          left: bounds.origin.x,
+          top: bounds.origin.y,
+        }}
+      >
+        <Text style={{
+          color: '#F00',
+          flex: 1,
+          position: 'absolute',
+          textAlign: 'center',
+          backgroundColor: 'transparent',
+        }}>{data}</Text>
+      </View>
+    </React.Fragment>
+  );
+  
+  render() {
+    return (
+      <RNCamera
+        ref={ref => { this.camera = ref; }}
+
+        style={{
+          flex: 1,
+          width: '100%',
+        }}
+        onGoogleVisionBarcodesDetected={this.barcodeRecognized}
+      >
+        {this.renderBarcodes()}
+      </RNCamera>
+    );
+  }
+}
